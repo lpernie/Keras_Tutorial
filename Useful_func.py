a1cd3fcf395eb3b796fb1fbbb2993ae6befcb55e
lpernie <mrluca87@gmail.com>
2017-04-21 17:46:59 -0500

    Fix

--- Useful_func.py
+++ Useful_func.py
@@ -178,3 +178,14 @@ def my_min(a, b):
 def my_max(a, b):
   if(a>b): return a
   else: return b
+
+class PDF(object):
+  def __init__(self, pdf, size=(200,200)):
+    self.pdf = pdf
+    self.size = size
+
+  def _repr_html_(self):
+    return '<iframe src={0} width={1[0]} height={1[1]}></iframe>'.format(self.pdf, self.size)
+
+  def _repr_latex_(self):
+    return r'\includegraphics[width=1.0\textwidth]{{{0}}}'.format(self.pdf)
